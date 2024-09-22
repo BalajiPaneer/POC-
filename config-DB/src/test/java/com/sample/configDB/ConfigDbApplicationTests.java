6f342a6bfa8ede07b399cd2a60fdf2acd0d6987e
BalajiPaneer <balajiinfo301@gmail.com>
2024-09-22 21:05:20 +0530

    Config server db

---
+++ config-DB/src/test/java/com/sample/configDB/ConfigDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.configDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
